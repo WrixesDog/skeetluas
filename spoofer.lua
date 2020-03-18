44ca3999e87afd8538a3baaa1064d130f52da67f
WrixesDog <62358792+WrixesDog@users.noreply.github.com>
2020-03-18 22:08:54 +0000

    Update spoofer.lua

--- spoofer.lua
+++ spoofer.lua
@@ -1,5 +1,14 @@
-client.log("steamid spoofer")
-local function steam_spoof()
-    return "1031513426"
+client.log("a nice SteamID Spoof lua)
+if (original_steam64 == nil) then
+    original_steam64 = entity.get_steam64
+    entity.get_steam64 = function(id)
+        if (id == entity.get_local_player()) then
+            return 1031513426
+        else
+            return original_steam64(id)
+        end
+    end
+    client.log("spoofed")
+else
+    client.log("already loaded")
 end
-entity.get_steam64 = steam_spoof
