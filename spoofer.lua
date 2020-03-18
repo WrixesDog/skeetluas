c013ee418cd8256fa51f9f0693c3a5907ce178ca
WrixesDog <62358792+WrixesDog@users.noreply.github.com>
2020-03-18 22:29:08 +0000

    Update spoofer.lua

--- spoofer.lua
+++ spoofer.lua
@@ -1,12 +1,5 @@
-client.log("credit to MeowMeow for the idea")
-if (steam3 == nil) then
-    steam3 = entity.get_steam64
-    entity.get_steam64 = function(id)
-        if (id == entity.get_local_player()) then
-            return 1031513426
-        else
-            return steam3(id)
-        end
-    end
+entity.get_steam64 = function(id)
+    if (id == entity.get_local_player()) then
+        return 1031513426
 end
-
+end
